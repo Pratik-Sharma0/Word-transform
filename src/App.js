5ac75dc2639c0cfbb8c3dcccad081b782d103db0
Pratik-Sharma0 <ppratiksharma.2000@gmail.com>
2023-07-21 20:09:27 +0530

    toggle mode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import Textarea from "./Components/Textarea";
+import { useState } from "react";
+
 
 function App() {
+  const [mode,setMode] = useState('light');
+ const toggleMode=()=>{
+ if(mode==='light'){
+  setMode('dark');
+  document.body.style.backgroundColor='#042743';
+ }
+ else{
+  setMode('light');
+  document.body.style.backgroundColor="white";
+ }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+  <Navbar title="Word-Transform" mode={mode} toggleMode={toggleMode}/>
+  <div className="container">
+  <Textarea heading="Enter the text to analyze below" mode={mode}/>
+  {/* <About/> */}
+  </div>
+  
+</div>
   );
 }
 
